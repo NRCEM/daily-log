4167a0fd799bdbdf0b2807e1e90a870647dfd891
NRCEM <anviehuynh@gmail.com>
2026-09-14 22:45:55 -0700

    refactor file I/O and improve CLI output

--- main.py
+++ main.py
@@ -2,39 +2,49 @@ import argparse
 import json
 from datetime import date, timedelta
 
-def add_log(text):  
+def load_data():
 
     try:
         with open("data.json", "r", encoding="utf-8") as f:
             data = json.load(f)
     except FileNotFoundError:
         data = []
+    return data
 
+def save_data(data):
+    
     with open("data.json", "w", encoding="utf-8") as f:
-        data.append(text)
         json.dump(data, f, indent = 2, ensure_ascii=False)
 
 
+def add_log(entry):  
+
+    data = load_data()
+    data.append(entry)
+    save_data(data)
+    print(f'Successfully added: {entry["text"]} on {entry["date"]}')
+
 def list_logs():
 
-    with open("data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        for d in data:
-            print(f'{d["date"]}: {d["text"]}')
+    data = load_data() 
+    print("Logs:")
+    for d in data:
+        print(f'{d["date"]}: {d["text"]}')
 
 
 def streak_logs(current):
 
-    with open("data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        dates = set()
-        streak = 0
-        for d in data:
-            dates.add(d["date"])
-        while current.isoformat() in dates:
-            streak += 1
-            current -= timedelta(days=1)
-        print(streak)
+    data = load_data()
+    dates = set()
+    streak = 0
+    for d in data:
+        dates.add(d["date"])
+    while current.isoformat() in dates:
+        streak += 1
+        current -= timedelta(days=1)
+    if streak == 1:
+        print("Current streak: 1 day")
+    else: print(f'Current streak: {streak} days')
 
 
 if __name__ == "__main__":
